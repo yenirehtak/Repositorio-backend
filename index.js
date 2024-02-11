6184016cc1674014c868adf2809213ae919a34c1
Katherine Castro <katherineycastro3@gmail.com>
2024-02-10 22:29:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors= require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+const about = require("./JSON/About.json");
+const portfolio = require("./JSON/Portfolio.json");
+
+app.get("/about", (req,res) => {
+    res.json(about)
+});
+
+app.get("/portfolio", (req,res) => {
+    res.json(portfolio)
+});
+
+app.listen(port, () => {
+    console.log(`server on : ${port}`)
+});
