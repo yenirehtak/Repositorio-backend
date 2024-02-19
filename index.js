cb07916b2bc13cfb85efdb27e1cd2746bbbebea7
Katherine Castro <katherineycastro3@gmail.com>
2024-02-19 01:53:57 -0400

    cambio

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors= require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+const about = require("./JSON/About.json");
+const portfolio = require("./JSON/Portfolio.json");
+
+app.get("/", (req,res) => {
+    res.send("mi primera web pls!!!") 
+});
+
+app.get("/about", (req,res) => {
+    res.json(about)
+});
+
+app.get("/portfolio", (req,res) => {
+    res.json(portfolio)
+});
+
+app.listen(port, () => {
+    console.log(`server on : ${port}`)
+});
